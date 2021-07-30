2c723cfee9198b7071a839a7c24c8dbc8f0e45b2
Arianna Masciolini <uzkamascio@gmail.com>
2021-07-30 17:34:13 +0200

    progress in iterating through images

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -24,3 +24,41 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function addPhotos(places) {
+  var baseUrl = "raw.githubusercontent.com/harisont/postcards-from-gothenburg/master/photos/"
+  places.forEach(place => {
+    var side = "a"
+    var imgUrl = baseUrl + place + "/" + side + ".jpg"
+    alert(imgUrl)
+  });
+}
+
+function main() {
+  // these are directory names and need to be kept updated. They are here to
+  // iterate through images because "good" old js doesn't have access to the
+  // filesystem... apparently
+  var places = [
+    //"alvborgsbron",
+    //"gamlestadens_resecentrum_och_varldslitteraturhus",
+    //"johanneberg_science_park",
+    //"lyktan",
+    //"chalmers_bibliotek",
+    //"garda_fabriker",
+    //"kuggen",
+    //"molndalsfallen",
+    //"eriksberg",
+    //"gotaverkens_kranar",
+    //"kvibergs_kasermer",
+    //"radisson_blu",
+    //"gamlestadens_cykelparkering",
+    //"gothia_towers",
+    //"lindholmen_science_park",
+    //"sveriges_radio",
+    //"gamlestadens_fabriker",
+    "hisingsbron"//,
+    //"lindholmens_tekniska_gymnasium"
+  ]
+  addPhotos(places);
+  currentSlide(1);
+}
